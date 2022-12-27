9298355e04723ed1e37570a3d4317e049b4ad2af
Rafiere <rafapgbr@gmail.com>
2022-12-27 10:37:08 -0300

    feat: adding methods.

--- api/index.js
+++ api/index.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const routes = require("./routes/index.js");
 
 const app = express();
-
-/* Estamos avisando para o Express que a biblioteca "Body Parser" fará o meio de campo entre as requisições e a aplicação, convertendo o corpo das requisições para o formato JSON. */
-app.use(bodyParser.json());
-
 const port = 3000;
 
-app.get("/teste", (req, res) => {
-  res.status(200).send({ message: "Olá!" });
-});
+routes(app);
 
 /* O Express estará escutando a porta 3000. */
 app.listen(port, () => {
   console.log(`O servidor está sendo executado na porta ${port}.`);
 });
+
+module.exports = app;
